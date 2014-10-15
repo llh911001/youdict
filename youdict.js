ba7924386862327649ad56c1d0b7bcf50809d9fb
Li linghao <lilh@shterm.com>
2014-10-15 11:23:14 +0800

    update package.json

--- youdict.js
+++ youdict.js
@@ -1,31 +1,31 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var chalk   = require('chalk');
 var word    = process.argv[2];
 var url     = 'http://dict.youdao.com/search?q=' + word;
 
 if(!word){
-    console.log('What are you looking for?');
     return;
 }
 
 request(url, function(error, response, body){
     if(error || response.statusCode !== 200){
-        console.error(error);
+        console.log(chalk.bold.red(error));
     }
     var $         = cheerio.load(body);
     var $phrs     = $('#phrsListTab');
     var $eg       = $('#bilingual');
 
-    console.log(word + ' ' + $('span.phonetic', $phrs).text());
+    console.log(chalk.bold.blue(word + ' ' + $('span.phonetic', $phrs).text()));
     console.log('');
     $('.trans-container>ul>li', $phrs).each(function(){
-        console.log($(this).text());
+        console.log(chalk.green($(this).text()));
     });
     $('ul>li', $eg).each(function(){
         console.log('');
         $('p', this).each(function(){
             if(!$(this).hasClass('example-via')){
-                console.log($(this).text().trim());
+                console.log(chalk.yellow($(this).text().trim()));
             }
         });
     });
